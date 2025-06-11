e5946223ddee52349bb0f906a22e20be6650a20c
CALEB OUEDRAOGO <calebouedraogo@macbookpro.mynetworksettings.com>
2025-06-11 12:01:32 -0400

    Building applications using the CLI

---
+++ HelloWorldFromCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello everybody");
+
+Console.WriteLine("Please enter your first name: ");
+string firstname = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name: ");
+string lastname = Console.ReadLine();
+
+Console.WriteLine($"Hello " + firstname + " " + lastname);
